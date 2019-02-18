1570e9f762176145b6e6777e1ca0ac65d71b7274
Vinh <vinhvt@robot.co.jp>
2019-02-18 21:38:42 +0700

    manage assemblies

---
+++ Script/Utils/RestartSceneOnGUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartSceneOnGUI : MonoBehaviour
+{
+    [SerializeField] private GUISkin skin;
+    [SerializeField] private GUIStyle style;
+    [SerializeField] private int width = 200;
+    [SerializeField] private int height = 70;
+    void OnGUI()
+    {
+        if (style != null)
+        {
+            GUI.skin = skin;
+        }
+
+        if (GUI.Button(new Rect(20, 40, width, height), "Reset"))
+        {
+            Application.LoadLevel(SceneManager.GetActiveScene().name);
+        }
+    }
+}
