bc8c63e87d18f854a964d84795bca125f6d17c94
Vinh <mrthanhvinh168@gmail.com>
2019-02-18 21:13:02 +0700

    clean

--- Assets/App/Scripts/Boards/IBoard.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace App.Scripts.Boards
-{
-    public interface IBoard
-    {
-        int Width { get; set; }
-        int Height { get; set; }
-        int MineCount { get; set; }
-    }
-}
