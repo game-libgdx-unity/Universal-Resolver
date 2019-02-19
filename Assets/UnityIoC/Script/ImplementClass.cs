01d471f83f294041cf1e0b45515fc060b7a791a9
DESKTOP-ONG9T0O\MSI <sdasd!@fd.ffd>
2019-02-19 16:44:20 +0700

    update implementClass

--- Assets/UnityIoC/Script/ImplementClass.cs
+++ Assets/UnityIoC/Script/ImplementClass.cs
@@ -1,10 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityIoC;
+using Object = UnityEngine.Object;
 
 [CreateAssetMenu(fileName = "TestConfiguration", menuName = "Test/TestConfiguration", order = 1)]
 public class ImplementClass :ScriptableObject
 {
-    [SerializeField] public Object[] classObjectsToLoad;
+    [SerializeField] public BindingData[] classObjectsToLoad;
+}
+
+[Serializable]
+public class BindingData
+{
+    [SerializeField] public Object AbstractType;
+    [SerializeField] public Object ImplementedType;
+    [SerializeField] public LifeCycle LifeCycle;
+    [SerializeField] public Object InjectInto;
 }
